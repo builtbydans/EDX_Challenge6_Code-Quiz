7984e4911483ac0c238ee44cb229fd1cd25e35b9
Danish Shafi <danish.shafi@live.co.uk>
2023-01-15 17:18:23 +0000

    Condition added to change start-screen if timer hits 0

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,23 @@
 // All DOM elements called from index.html and stored
 var startBtn = document.querySelector('#start');
+var mainTitle = document.querySelector('#main-title');
+var mainText = document.querySelector('#main-text');
 var timer = document.querySelector('#time');
 
 // Function that starts the timer when user presses start button
 function startTimer() {
   startBtn.addEventListener('click', function() {
     var time = 60;
-
     var downTimer = setInterval(function(){
       timer.textContent = time;
       time--;
-      if(time <= 0)
-          clearInterval(downTimer);
-      },1000);
+
+      if(time < 0) {
+        clearInterval(downTimer);
+        mainTitle.textContent = 'Oops! You ran out of time!'
+        mainText.textContent += ' Press Start to try again!'
+      }
+    },1000);
   });
 }
 
