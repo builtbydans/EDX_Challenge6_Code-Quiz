65708781637cc7f0ffa01b1a44c7051ab4723f5e
Danish Shafi <danish.shafi@live.co.uk>
2023-01-15 17:26:38 +0000

    hiding start screen when user clicks start button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,7 +2,9 @@
 var startBtn = document.querySelector('#start');
 var mainTitle = document.querySelector('#main-title');
 var mainText = document.querySelector('#main-text');
+var startScreen = document.querySelector('#start-screen');
 var timer = document.querySelector('#time');
+var questionScreen = document.querySelector('#questions');
 
 // Function that starts the timer when user presses start button
 function startTimer() {
@@ -11,15 +13,22 @@ function startTimer() {
     var downTimer = setInterval(function(){
       timer.textContent = time;
       time--;
+      startScreen.className = 'hide';
 
       if(time < 0) {
         clearInterval(downTimer);
-        mainTitle.textContent = 'Oops! You ran out of time!'
-        mainText.textContent += ' Press Start to try again!'
+        startScreen.className = 'show'
+        mainTitle.textContent = 'Oops! You ran out of time!';
+        mainText.textContent += ' Press Start to try again!';
       }
     },1000);
   });
+};
+
+// Function that begins the questions rounds
+function beginQuestions() {
+  startTimer();
 }
 
 // Calling functions
-startTimer();
+beginQuestions()
--- pseudo.md
+++ pseudo.md
@@ -2,4 +2,6 @@ Step 1: Apply a click eventListener to the start button.
 
 Step 2: When the start button eventListener fires, change the Timer element to countdown from 60.
 
-Step 3:
+Step 3: When user presses start button, screen changes and shows questions in succession.
+
+Step 4: Apply logic that when user makes a mistake, timer reduces by 10 seconds.
